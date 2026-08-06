bf751eb328687dced586833bc58d2e36d42f1158
opeolluwa <adefemiadeoye@yahoo.com>
2026-08-06 22:52:40 +0100

    chore

--- projects/mule/src/main.cpp
+++ projects/mule/src/main.cpp
@@ -1,5 +1,14 @@
 #include <cstdio>
+#include <iostream>
+#include <string>
 
-int main(){
-    std::printf("Hello World");
+int main() {
+  std::string baseDir{};
+
+  std::printf("Enter the base directory: ");
+  std::cin >> baseDir;
+
+  std::cout << " you entered " << baseDir << std::endl;
+  std::string directories[4]{"src", "include", "external", "assets"};
+
 }
