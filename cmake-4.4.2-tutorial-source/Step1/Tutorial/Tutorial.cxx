d72cbc2e97a9898e0297ffe4f283a1f8857a08b4
opeolluwa <adefemiadeoye@yahoo.com>
2026-08-08 21:09:57 +0100

    link executables and libraries

--- cmake-4.4.2-tutorial-source/Step1/Tutorial/Tutorial.cxx
+++ cmake-4.4.2-tutorial-source/Step1/Tutorial/Tutorial.cxx
@@ -1,12 +1,11 @@
 // A simple program that computes the square root of a number
+#include <MathFunctions.h>
 #include <cmath>
 #include <iostream>
 #include <string>
-
 // TODO8: Include the MathFunctions header
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
   if (argc < 2) {
     std::cout << "Usage: " << argv[0] << " number" << std::endl;
     return 1;
@@ -17,7 +16,7 @@ int main(int argc, char* argv[])
 
   // TODO9: Use the mathfunctions::sqrt function
   // calculate square root
-  double const outputValue = std::sqrt(inputValue);
+  double const outputValue = mathfunctions::sqrt(inputValue);
   std::cout << "The square root of " << inputValue << " is " << outputValue
             << std::endl;
 }
