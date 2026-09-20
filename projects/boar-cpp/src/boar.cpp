c01dcc7b518eaa1334e6be7845df18e273403017
Adefemi Adeoye <adefemiadeoye@yahoo.com>
2026-09-20 22:48:39 +0100

    chore: add generating CMAKE and README files

--- projects/boar-cpp/src/boar.cpp
+++ projects/boar-cpp/src/boar.cpp
@@ -1,9 +1,15 @@
 #include "boar.hpp"
+#include <filesystem>
+#include <fstream>
 #include <string>
 #include <string_view>
 
 using boar::Boar;
 using boar::parse_input;
+using std::string;
+using std::string_view;
+
+namespace fs = std::filesystem;
 
 Boar::Boar(const boar::Options &opts) : option(opts) {}
 
@@ -11,20 +17,45 @@ boar::Options boar::Boar::parse_options() {
   boar::Options options;
 
   options.use_cmake = parse_input<bool>("Use CMake?");
-  options.app_name = parse_input<std::string>("App name");
+  options.app_name = parse_input<string>("App name");
   if (options.use_cmake) {
-    options.cmake_version = parse_input<std::string>("Cmake version");
+    options.cmake_version = parse_input<string>("Cmake version");
     options.add_gtest = parse_input<bool>("add gtest");
   }
   options.add_readme = parse_input<bool>("add readme?");
+
   return options;
 }
 
 void Boar::exec() {
-  if (option.use_cmake) {
-    build_cmake_template();
-  }
+  if (option.use_cmake)
+    file_exist_or_create("CMakeLists.txt", build_cmake_template());
+
+  if (option.add_readme)
+    file_exist_or_create("README.md",
+                         std::format(README_TEMPLATE, option.app_name));
 }
-void Boar::build_cmake_template() {
-    auto cmake_template = std::format(boar::CMAKE_TEMPLATE, option.cmake_version, )
+
+void Boar::file_exist_or_create(std::string_view file_name,
+                                std::string_view content) {
+  fs::path filepath = fs::current_path() / file_name;
+  bool can_write =
+      !fs::exists(filepath) ||
+      parse_input<bool>(std::format("{} exists, overwrite?", file_name));
+
+  if (can_write)
+    std::ofstream(filepath) << content;
+}
+
+std::string Boar::build_cmake_template() {
+  auto cmake_gtest_template_addon =
+      std::format(boar::GTEST_TEMPLATE, option.app_name);
+
+  if (!option.add_gtest)
+    cmake_gtest_template_addon = "";
+
+  return std::format(boar::CMAKE_TEMPLATE,
+                     option.cmake_version.value_or(
+                         "") /*todo: parse the currntly cmake version */,
+                     option.app_name, cmake_gtest_template_addon);
 }
--- projects/boar-cpp/src/boar.hpp
+++ projects/boar-cpp/src/boar.hpp
@@ -23,27 +23,91 @@ constexpr auto CMAKE_TEMPLATE = R"(cmake_minimum_required(VERSION {0})
     set(CMAKE_CXX_STANDARD 20)
     set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
-    add_executable(${PROJECT_NAME})
-    target_sources(${PROJECT_NAME} PRIVATE src/main.cpp src/{1}.cpp src/{1}.hpp)
-    target_link_libraries(${PROJECT_NAME} PRIVATE)
+    add_executable(${{PROJECT_NAME}})
+    target_sources(${{PROJECT_NAME}} PRIVATE src/main.cpp src/{1}.cpp src/{1}.hpp)
+    target_link_libraries(${{PROJECT_NAME}} PRIVATE)
+
+    {2}
 )";
 
-
 constexpr auto GTEST_TEMPLATE = R"(enable_testing()
-    add_executable(${PROJECT_NAME}_tests)
-    target_sources(${PROJECT_NAME}_tests PRIVATE tests/boar_spec.cpp src/boar.cpp src/boar.hpp)
-    target_include_directories(${PROJECT_NAME}_tests PRIVATE ${CMAKE_CURRENT_SOURCE_DIR})
-    target_link_libraries(${PROJECT_NAME}_tests PRIVATE GTest::gtest_main  termcolor::termcolor)
+    add_executable(${{PROJECT_NAME}}_tests)
+    target_sources(${{PROJECT_NAME}}_tests PRIVATE tests/{0}_spec.cpp src/{0}.cpp src/{0}.hpp)
+    target_include_directories(${{PROJECT_NAME}}_tests PRIVATE ${{CMAKE_CURRENT_SOURCE_DIR}})
+    target_link_libraries(${{PROJECT_NAME}}_tests PRIVATE GTest::gtest_main)
 
     FetchContent_Declare(
       googletest
       URL https://github.com/google/googletest/archive/03597a01ee50ed33e9dfd640b249b4be3799d395.zip
     )
-    FetchContent_MakeAvailable(termcolor googletest)
+    FetchContent_MakeAvailable(googletest)
     include(GoogleTest)
-    gtest_discover_tests(${PROJECT_NAME}_tests)
+    gtest_discover_tests(${{PROJECT_NAME}}_tests)
 )";
 
+constexpr auto README_TEMPLATE = R"(# {0}
+    
+    Simple overview of use/purpose.
+    
+    ## Description
+    
+    An in-depth paragraph about your project and overview of use.
+    
+    ## Getting Started
+    
+    ### Dependencies
+    
+    * Describe any prerequisites, libraries, OS version, etc., needed before installing program.
+    * ex. Windows 10
+    
+    ### Installing
+    
+    * How/where to download your program
+    * Any modifications needed to be made to files/folders
+    
+    ### Executing program
+    
+    * How to run the program
+    * Step-by-step bullets
+    ```
+    code blocks for commands
+    ```
+    
+    ## Help
+    
+    Any advise for common problems or issues.
+    ```
+    command to run if program contains helper info
+    ```
+    
+    ## Authors
+    
+    Contributors names and contact info
+    
+    ex. Dominique Pizzie
+    ex. [@DomPizzie](https://twitter.com/dompizzie)
+    
+    ## Version History
+    
+    * 0.2
+        * Various bug fixes and optimizations
+        * See [commit change]() or See [release history]()
+    * 0.1
+        * Initial Release
+    
+    ## License
+    
+    This project is licensed under the [NAME HERE] License - see the LICENSE.md file for details
+    
+    ## Acknowledgments
+    
+    Inspiration, code snippets, etc.
+    * [awesome-readme](https://github.com/matiassingers/awesome-readme)
+    * [PurpleBooth](https://gist.github.com/PurpleBooth/109311bb0361f32d87a2)
+    * [dbader](https://github.com/dbader/readme-template)
+    * [zenorocha](https://gist.github.com/zenorocha/4526327)
+    * [fvcproductions](https://gist.github.com/fvcproductions/1bfc2d4aecb01a834b46)
+            )";
 struct Options {
   std::vector<std::string> dirs;
   std::string app_name;
@@ -56,10 +120,9 @@ struct Options {
 class Boar {
 private:
   Options option;
-  std::string render_cmake_content();
-  bool cmake_file_exists();
-  bool readme_exist();
-  void build_cmake_template();
+  std::string build_cmake_template();
+  void file_exist_or_create(std::string_view file_name,
+                            std::string_view content);
 
 public:
   Boar() = default;
